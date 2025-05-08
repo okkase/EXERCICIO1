af9d9d64d2eb7f18d35a64e1c61537c57ec55052
okkase <felipe.costabeber.s@gmail.com>
2025-05-08 12:55:20 -0300

    quase terminando

--- app/src/main/java/com/example/exercicio1/MainActivity.kt
+++ app/src/main/java/com/example/exercicio1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.exercicio1
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+    val itens = listOf("masculino", "feminino")
+
+
+        val adapter = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,itens)
+        binding.spinnerSexo.adapter = adapter
+
+        binding.buttonCalc.sentOnClickListener{
+            val sexoSelecionado = binding.spinnerSexo.selectedItem as String
+            val idade = binding.editTextIdade.text.toString().toLongOrNull()
+            var resultado: Long
+            if (idade!= null) {
+                if (sexoSelecionado.trim() == "masculino" ) {
+                    resultado = 65 - idade
+
+                } else {
+                    resultado = 62 - idade
+                }
+                binding.textResultado.text = "$resultado"
+            }
+
+        }
         }
     }
